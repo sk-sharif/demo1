94e283dab0f81c378427796c1b54c53fcbde14bb
sk-sharif <60419485+sk-sharif@users.noreply.github.com>
2021-07-26 15:17:47 +0530

    Update building.groovy

--- vars/building.groovy
+++ vars/building.groovy
@@ -1,6 +1,6 @@
 def call(body) {
   def pipelineParams= [:]
-  body.resolveStrategy = Closure.DELEGATE_FIRST
+//   body.resolveStrategy = Closure.DELEGATE_FIRST
   body.delegate = pipelineParams
   body()
   
