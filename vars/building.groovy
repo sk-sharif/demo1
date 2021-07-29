01764e5933d33bcac4bd1a4545fc8e22ced66144
sk-sharif <60419485+sk-sharif@users.noreply.github.com>
2021-07-29 11:44:04 +0530

    Update building.groovy

--- vars/building.groovy
+++ vars/building.groovy
@@ -1,17 +1,9 @@
-def call(body) {
-  def pipelineParams= [:]
-  body.resolveStrategy = Closure.DELEGATE_FIRST
-  body.delegate = pipelineParams
-  body()
-  
-  pipeline{
-    agent any 
-    stages {
-      stage('buildig') {
-        steps {
-          sh "echo ${pipelineParams.name} building"
+def call(Map config [:]) {
+  stage('calling groovy script') {
+      steps {
+        script {
+          echo 'hello' 
         }
       }
     }
-  }
 }
