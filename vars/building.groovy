67d9032c20ef4fa6abe03c9032fc45255272cc0b
sk-sharif <60419485+sk-sharif@users.noreply.github.com>
2021-08-04 12:49:02 +0530

    Update building.groovy

--- vars/building.groovy
+++ vars/building.groovy
@@ -1,22 +1,5 @@
-def call(String repoUrl) {
-//    pipeline{
-//        agent any
-//        stages {
-//            stage("Tools initialization") {
-//                steps {
-//                   echo "hello ${repoUrl}"                       
-//                   echo "Hi ${repoUrl}"
-//                }
-//            }
-//        }
-//    }
-   stages {
-   stage('building') {
-      steps {
-        script {
-           echo "HI ${repoUrl}"
-        }
-      }
-    }
-   }
+def add(x){
+   sh '''
+      echo ${x}
+   '''
 }
