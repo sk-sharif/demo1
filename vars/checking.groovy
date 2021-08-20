b4ffe4f7f5be9eb85f6ba2e91a561429630d19d7
sk-sharif <60419485+sk-sharif@users.noreply.github.com>
2021-08-20 13:45:29 +0530

    Update checking.groovy

--- vars/checking.groovy
+++ vars/checking.groovy
@@ -6,3 +6,14 @@ def calling(Map config = [:]) {
     echo "checking stage 2 Checked"
 }
 
+def calling1(Map config = [:]) {
+    stage("checking stage 1") { 
+        echo "checking stage 1 Checked"
+    }
+}
+
+def calling2(Map config = [:]) {
+    stage("checking stage 2") {
+        echo "checking stage 2 Checked"
+    }
+}
