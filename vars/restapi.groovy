29b5466220dd5da5db09e25f990817d8e932d3b7
sk-sharif <60419485+sk-sharif@users.noreply.github.com>
2021-09-06 15:02:27 +0530

    Update restapi.groovy

--- vars/restapi.groovy
+++ vars/restapi.groovy
@@ -1,35 +1,54 @@
-def call(Map config = [:]) {
-  echo "checking 1"
-  def connection = new URL("http://54.36.230.136:2000/api/machine/list").openConnection();
-  echo "checking 2" 
+def listsOfMachine(Map config = [:]) {
+  //GET
+  def connection = new URL("http://54.36.230.136:2000/api/machine/list").openConnection(); 
   connection.setRequestMethod("GET");
-  echo "checking 3"
   connection.doOutput = false;
-  echo "checking 4"
   connection.connect();
-  echo "checking 5"
   println(connection.content.text);
+}
+
+def statusOfMachine(Map config = [:]) {
+  // GET
+  def connection = new URL("http://54.36.230.136:2000/api/machine/status/test1").openConnection(); 
+  connection.setRequestMethod("GET");
+  connection.doOutput = false;
+  connection.connect();
+  println(connection.content.text);
+}
+
+def createMachine(Map config = [:]) {
+  //POST
+  try {
+	  def post = new URL("54.36.230.136:2000/api/machine/create/test4?config=test-infra").openConnection();
+	  def message = '{"message":"this is a message"}'
+    post.setRequestMethod("POST")
+	  post.setDoOutput(true)
+	  post.setRequestProperty("Content-Type", "application/json")
+	  post.getOutputStream().write(message.getBytes("UTF-8"));
+	  def postRC = post.getResponseCode();
+	  println(postRC);
+	  if(postRC.equals(200)) {
+	    println("Machine is being created.");
+	  }  else {
+	  	println("Machin cannot be created!")
+	  }
+	} catch (Exception ex) {
+		println("Catching the exception");
+		 println(ex.toString());
+         println(ex.getMessage());
+	}
 }
 
 def post(Map config = [:]) {
   // POST
-  echo "checking 1"
   def post = new URL("http://localhost:8012/anything/srijal").openConnection();
-  echo "checking 2"
   def message = '{"message":"this is a message"}'
-  echo "checking 3"
   post.setRequestMethod("POST")
-  echo "checking 4"
   post.setDoOutput(true)
-  echo "checking 5"
   post.setRequestProperty("Content-Type", "application/json")
-  echo "checking 6"
   post.getOutputStream().write(message.getBytes("UTF-8"));
-  echo "checking 7"
   def postRC = post.getResponseCode();
-  echo "checking 8"
   println(postRC);
-  echo "checking 9"
   if(postRC.equals(200)) {
     println(post.getInputStream().getText());
   }  
