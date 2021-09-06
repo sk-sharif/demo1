e1bb8242b93381801537a0212c9c40f64cdf4499
sk-sharif <60419485+sk-sharif@users.noreply.github.com>
2021-09-06 11:44:37 +0530

    Update restapi.groovy

--- vars/restapi.groovy
+++ vars/restapi.groovy
@@ -1,6 +1,6 @@
 def call(Map config = [:]) {
   echo "checking 1"
-  def connection = new URL("http://localhost:8012/drip").openConnection();
+  def connection = new URL("http://localhost:8012/get").openConnection();
   echo "checking 2"
 //   def getRC = get.getResponseCode();
 //   echo "checking 3"
