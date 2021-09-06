50443d67b8e4686ac4d54ccd2c64bf581112a3a3
sk-sharif <60419485+sk-sharif@users.noreply.github.com>
2021-09-06 15:06:19 +0530

    Update restapi.groovy

--- vars/restapi.groovy
+++ vars/restapi.groovy
@@ -19,13 +19,21 @@ def statusOfMachine(Map config = [:]) {
 def createMachine(Map config = [:]) {
   //POST
   try {
+	  echo "1"
 	  def post = new URL("54.36.230.136:2000/api/machine/create/test4?config=test-infra").openConnection();
+	  echo "2"
 	  def message = '{"message":"this is a message"}'
-    post.setRequestMethod("POST")
+    `	  echo "3"
+	  post.setRequestMethod("POST")
+	  echo "4"
 	  post.setDoOutput(true)
+	  echo "5"
 	  post.setRequestProperty("Content-Type", "application/json")
+	  echo "6"
 	  post.getOutputStream().write(message.getBytes("UTF-8"));
+	  echo "7"
 	  def postRC = post.getResponseCode();
+	  echo "8"
 	  println(postRC);
 	  if(postRC.equals(200)) {
 	    println("Machine is being created.");
