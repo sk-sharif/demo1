ff6ba436519ba522f51455668b9fcb222c489af0
sk-sharif <60419485+sk-sharif@users.noreply.github.com>
2021-09-06 17:14:26 +0530

    Update restapi.groovy

--- vars/restapi.groovy
+++ vars/restapi.groovy
@@ -111,7 +111,7 @@ def updateMachine(Map config = [:]) {
 
 def post(Map config = [:]) {
   // POST
-  def post = new URL("http://54.36.230.136:2000/api/machine/start/test1").openConnection();
+  def post = new URL("http://54.36.230.136:2000/api/machine/stop/test1").openConnection();
   def message = '{"message":"this is a message"}'
   post.setRequestMethod("POST")
   post.setDoOutput(true)
@@ -120,6 +120,6 @@ def post(Map config = [:]) {
   def postRC = post.getResponseCode();
   println(postRC);
   if(postRC.equals(200)) {
-    println("Machine is Started.");
+    println("Machine is Stopped.");
   }  
 }
