9f3f6759ba405755c2437e7d1640d255a53b9265
sk-sharif <60419485+sk-sharif@users.noreply.github.com>
2021-09-06 17:24:47 +0530

    Update restapi.groovy

--- vars/restapi.groovy
+++ vars/restapi.groovy
@@ -19,7 +19,7 @@ def statusOfMachine(Map config = [:]) {
 def createMachine(Map config = [:]) {
   //POST
 	try {
-	  def post = new URL("http://54.36.230.136:2000/api/machine/create/test5?config=test-infra").openConnection();
+	  def post = new URL("http://54.36.230.136:2000/api/machine/create/test6?config=test-infra").openConnection();
 	  def message = '{"message":"this is a message"}'
 	  post.setRequestMethod("POST")
 	  post.setDoOutput(true)
@@ -42,7 +42,7 @@ def createMachine(Map config = [:]) {
 def startMachine(Map config = [:]) {
   //POST
 	try {
-	  def post = new URL("http://54.36.230.136:2000/api/machine/start/test1").openConnection();
+	  def post = new URL("http://54.36.230.136:2000/api/machine/start/test6").openConnection();
 	  def message = '{"message":"this is a message"}'
     	  post.setRequestMethod("POST")
 	  post.setDoOutput(true)
@@ -62,28 +62,6 @@ def startMachine(Map config = [:]) {
 	}
 }
 
-def stopMachine(Map config = [:]) {
-  //POST
-	try {
-	  def post = new URL("http://54.36.230.136:2000/api/machine/stop/test1").openConnection();
-	  def message = '{"message":"this is a message"}'
-    	  post.setRequestMethod("POST")
-	  post.setDoOutput(true)
-	  post.setRequestProperty("Content-Type", "application/json")
-	  post.getOutputStream().write(message.getBytes("UTF-8"));
-	  def postRC = post.getResponseCode();
-	  println(postRC);
-	  if(postRC.equals(200)) {
-	    println("Machine is Stopped.");
-	  }  else {
-	  	println("Machine is cannot be Stopped!")
-	  }
-	} catch (Exception ex) {
-		println("Catching the exception");
-		 println(ex.toString());
-         println(ex.getMessage());
-	}
-}
 
 def updateMachine(Map config = [:]) {
   //POST
@@ -108,6 +86,28 @@ def updateMachine(Map config = [:]) {
 	}
 }
 
+def stopMachine(Map config = [:]) {
+  //POST
+	try {
+	  def post = new URL("http://54.36.230.136:2000/api/machine/stop/test6").openConnection();
+	  def message = '{"message":"this is a message"}'
+    	  post.setRequestMethod("POST")
+	  post.setDoOutput(true)
+	  post.setRequestProperty("Content-Type", "application/json")
+	  post.getOutputStream().write(message.getBytes("UTF-8"));
+	  def postRC = post.getResponseCode();
+	  println(postRC);
+	  if(postRC.equals(200)) {
+	    println("Machine is Stopped.");
+	  }  else {
+	  	println("Machine is cannot be Stopped!")
+	  }
+	} catch (Exception ex) {
+		println("Catching the exception");
+		 println(ex.toString());
+         println(ex.getMessage());
+	}
+}
 
 def post(Map config = [:]) {
   // POST
