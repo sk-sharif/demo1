2c02097b98b62660f92c07433280afcfa8e88ea1
sk-sharif <60419485+sk-sharif@users.noreply.github.com>
2021-09-06 10:34:13 +0530

    Update restapi.groovy

--- vars/restapi.groovy
+++ vars/restapi.groovy
@@ -1,12 +1,20 @@
 def call(Map config = [:]) {
   echo "checking 1"
-  def get = new URL("http://localhost:8012/post").openConnection();
+  def connection = new URL("http://localhost:8012/get").openConnection();
+//   echo "checking 2"
+//   def getRC = get.getResponseCode();
+//   echo "checking 3"
+//   println(getRC);
+//   echo "checking 4"
+//   if(getRC.equals(200)) {
+//     println(get.getInputStream().getText());
+//   }
   echo "checking 2"
-  def getRC = get.getResponseCode();
+  connection.setRequestMethod("GET")
   echo "checking 3"
-  println(getRC);
+  connection.doOutput = false
   echo "checking 4"
-  if(getRC.equals(200)) {
-    println(get.getInputStream().getText());
-  }
+  connection.connect()
+  echo "checking 5"
+  connection.content.text
 }
