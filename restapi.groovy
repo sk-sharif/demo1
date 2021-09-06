6dc38c8e2497024b92b72c07f50010e9c6ee2f93
sk-sharif <60419485+sk-sharif@users.noreply.github.com>
2021-09-06 09:48:30 +0530

    Create restapi.groovy

---
+++ restapi.groovy
@@ -0,0 +1,12 @@
+def call(Map config = [:]) {
+  echo "checking 1"
+  def get = new URL("http://localhost:8012/get").openConnection();
+  echo "checking 2"
+  def getRC = get.getResponseCode();
+  echo "checking 3"
+  println(getRC);
+  echo "checking 4"
+  if(getRC.equals(200)) {
+    println(get.getInputStream().getText());
+  }
+}
