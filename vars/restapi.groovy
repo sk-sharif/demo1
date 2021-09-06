6d11a2e5c6cffed0d7c3e74645af1da196f64052
sk-sharif <60419485+sk-sharif@users.noreply.github.com>
2021-09-06 13:56:01 +0530

    Update restapi.groovy

--- vars/restapi.groovy
+++ vars/restapi.groovy
@@ -16,40 +16,21 @@ def post(Map config = [:]) {
   echo "checking 1"
   def connection = new URL("http://localhost:8012/anything/srijal").openConnection();
   echo "checking 2"
-//   def message = '{"message":"this is a message"}'
-//   echo "checking 3"
-//   post.setRequestMethod("POST")
-//   echo "checking 4"
-//   post.setDoOutput(true)
-//   echo "checking 5"
-//   post.setRequestProperty("Content-Type", "application/json")
-//   echo "checking 6"
-//   post.getOutputStream().write(message.getBytes("UTF-8"));
-//   echo "checking 7"
-//   def postRC = post.getResponseCode();
-//   echo "checking 8"
-//   println(postRC);
-//   echo "checking 9"
-//   if(postRC.equals(200)) {
-//     println(post.getInputStream().getText());
-//   }
-  
-  connection.setRequestMethod("POST")
-        connection.doOutput = true
-        connection.outputStream.withWriter{
-            it.write(postContent)
-            it.flush()
-        }
-        connection.connect()
-
-        try {
-            println(connection.content.text)
-        } catch (IOException e) {
-            try {
-                ((HttpURLConnection)connection).errorStream.text
-            } catch (Exception ignored) {
-                throw e
-            }
-        }
-  
+  def message = '{"message":"this is a message"}'
+  echo "checking 3"
+  post.setRequestMethod("POST")
+  echo "checking 4"
+  post.setDoOutput(true)
+  echo "checking 5"
+  post.setRequestProperty("Content-Type", "application/json")
+  echo "checking 6"
+  post.getOutputStream().write(message.getBytes("UTF-8"));
+  echo "checking 7"
+  def postRC = post.getResponseCode();
+  echo "checking 8"
+  println(postRC);
+  echo "checking 9"
+  if(postRC.equals(200)) {
+    println(post.getInputStream().getText());
+  }  
 }
