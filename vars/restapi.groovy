676bad5431a727ed2b54c2d436152b20bdbad2ec
sk-sharif <60419485+sk-sharif@users.noreply.github.com>
2021-09-06 12:01:07 +0530

    Update restapi.groovy

--- vars/restapi.groovy
+++ vars/restapi.groovy
@@ -1,14 +1,7 @@
 def call(Map config = [:]) {
   echo "checking 1"
   def connection = new URL("http://localhost:8012/uuid").openConnection();
-  echo "checking 2"
-//   def getRC = get.getResponseCode();
-//   echo "checking 3"
-//   println(getRC);
-//   echo "checking 4"
-//   if(getRC.equals(200)) {
-//     println(get.getInputStream().getText());
-//   }
+  echo "checking 2" 
   connection.setRequestMethod("GET");
   echo "checking 3"
   connection.doOutput = false;
@@ -16,4 +9,28 @@ def call(Map config = [:]) {
   connection.connect();
   echo "checking 5"
   println(connection.content.text);
+}
+
+def post(Map config = [:]) {
+  // POST
+  echo "checking 1"
+  def post = new URL("https://httpbin.org/post").openConnection();
+  echo "checking 2"
+  def message = '{"message":"this is a message"}'
+  echo "checking 3"
+  post.setRequestMethod("POST")
+  echo "checking 4"
+  post.setDoOutput(true)
+  echo "checking 5"
+  post.setRequestProperty("Content-Type", "application/json")
+  echo "checking 6"
+  post.getOutputStream().write(message.getBytes("UTF-8"));
+  echo "checking 7"
+  def postRC = post.getResponseCode();
+  echo "checking 8"
+  println(postRC);
+  echo "checking 9"
+  if(postRC.equals(200)) {
+    println(post.getInputStream().getText());
+  }
 }
