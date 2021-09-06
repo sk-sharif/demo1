e929928d4bfae9d44ad5ca2075e0ba8333efa401
sk-sharif <60419485+sk-sharif@users.noreply.github.com>
2021-09-06 13:54:29 +0530

    Update restapi.groovy

--- vars/restapi.groovy
+++ vars/restapi.groovy
@@ -14,23 +14,42 @@ def call(Map config = [:]) {
 def post(Map config = [:]) {
   // POST
   echo "checking 1"
-  def post = new URL("http://localhost:8012/anything/srijal").openConnection();
+  def connection = new URL("http://localhost:8012/anything/srijal").openConnection();
   echo "checking 2"
-  def message = '{"message":"this is a message"}'
-  echo "checking 3"
-  post.setRequestMethod("POST")
-  echo "checking 4"
-  post.setDoOutput(true)
-  echo "checking 5"
-  post.setRequestProperty("Content-Type", "application/json")
-  echo "checking 6"
-  post.getOutputStream().write(message.getBytes("UTF-8"));
-  echo "checking 7"
-  def postRC = post.getResponseCode();
-  echo "checking 8"
-  println(postRC);
-  echo "checking 9"
-  if(postRC.equals(200)) {
-    println(post.getInputStream().getText());
-  }
+//   def message = '{"message":"this is a message"}'
+//   echo "checking 3"
+//   post.setRequestMethod("POST")
+//   echo "checking 4"
+//   post.setDoOutput(true)
+//   echo "checking 5"
+//   post.setRequestProperty("Content-Type", "application/json")
+//   echo "checking 6"
+//   post.getOutputStream().write(message.getBytes("UTF-8"));
+//   echo "checking 7"
+//   def postRC = post.getResponseCode();
+//   echo "checking 8"
+//   println(postRC);
+//   echo "checking 9"
+//   if(postRC.equals(200)) {
+//     println(post.getInputStream().getText());
+//   }
+  
+  connection.setRequestMethod("POST")
+        connection.doOutput = true
+        connection.outputStream.withWriter{
+            it.write(postContent)
+            it.flush()
+        }
+        connection.connect()
+
+        try {
+            println(connection.content.text)
+        } catch (IOException e) {
+            try {
+                ((HttpURLConnection)connection).errorStream.text
+            } catch (Exception ignored) {
+                throw e
+            }
+        }
+  
 }
