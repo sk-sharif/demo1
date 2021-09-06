9bd8f2acac346a2a2c9069abe730930d91ef9faa
sk-sharif <60419485+sk-sharif@users.noreply.github.com>
2021-09-06 14:00:06 +0530

    Update restapi.groovy

--- vars/restapi.groovy
+++ vars/restapi.groovy
@@ -1,6 +1,6 @@
 def call(Map config = [:]) {
   echo "checking 1"
-  def connection = new URL("http://localhost:8012/delay/1").openConnection();
+  def connection = new URL("http://localhost:8012/get").openConnection();
   echo "checking 2" 
   connection.setRequestMethod("GET");
   echo "checking 3"
