8d844398f765e4e012779a670882b8180566a073
sk-sharif <60419485+sk-sharif@users.noreply.github.com>
2021-10-01 14:08:52 +0530

    Update checking.groovy

--- vars/checking.groovy
+++ vars/checking.groovy
@@ -1,20 +1,22 @@
+name = "sharif"
+
 def call(Map config = [:]) {
-    echo "checking stage 1 Checked"
+    echo "checking stage 1 Checked ${name}"
 }
 
 def calling(Map config = [:]) {
-    echo "checking stage 2 Checked"
+    echo "checking stage 2 Checked ${name}"
 }
 
 def calling1(Map config = [:]) {
     stage("checking stage 1") { 
-        echo "checking stage 1 Checked"
+        echo "checking stage 1 Checked ${name}"
     }
 }
 
 def calling2(Map config = [:]) {
     stage("checking stage 2") {
-        echo "checking stage 2 Checked"
+        echo "checking stage 2 Checked ${name}"
     }
 }
 
