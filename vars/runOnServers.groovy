32dab96aefec0c6356fd15ed71e4ce65a2d104a0
sk-sharif <60419485+sk-sharif@users.noreply.github.com>
2021-10-24 00:42:19 +0530

    Update runOnServers.groovy

--- vars/runOnServers.groovy
+++ vars/runOnServers.groovy
@@ -11,25 +11,28 @@ def call(Map config = [:]) {
     },
     "StageB": {
       stage("stage B") {
+        when {
+          changeset "vars/**"
+        }
         echo "This is branch b"
       }
     }
   )
 }
 
-@NonCPS
-def changeLogs() {
-    def author = ""
-def changeSet = currentBuild.rawBuild.changeSets               
-for (int i = 0; i < changeSet.size(); i++) 
-{
-   def entries = changeSet[i].items;
-   for (i = 0; i < changeSet.size(); i++) 
-            {
-                       entries = changeSet[i].items;
-                       def entry = entries[0]
-                       author += "${entry.author}"
-            } 
- }
- print author;
-}
+// @NonCPS
+// def changeLogs() {
+//     def author = ""
+// def changeSet = currentBuild.rawBuild.changeSets               
+// for (int i = 0; i < changeSet.size(); i++) 
+// {
+//    def entries = changeSet[i].items;
+//    for (i = 0; i < changeSet.size(); i++) 
+//             {
+//                        entries = changeSet[i].items;
+//                        def entry = entries[0]
+//                        author += "${entry.author}"
+//             } 
+//  }
+//  print author;
+// }
