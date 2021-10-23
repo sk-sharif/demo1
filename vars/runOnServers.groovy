943333b7e2e9fc18874c3ed213860f59c54a9478
sk-sharif <60419485+sk-sharif@users.noreply.github.com>
2021-10-23 23:25:31 +0530

    Update runOnServers.groovy

--- vars/runOnServers.groovy
+++ vars/runOnServers.groovy
@@ -1,18 +1,14 @@
 def call(Map config = [:]) {
  parallel({
-   SanJose: {
        node {
          stage('SanJose') {
            echo "sanjose"
          }
        } 
-     }
-   Dallas: {
     node {
      stage('Dallas') {          
       echo "dallas"
      }
     }
-   } 
   })
 }
