7e9e151257f4675e3cf1422a1f2f234b5920bbce
sk-sharif <60419485+sk-sharif@users.noreply.github.com>
2021-10-24 00:12:39 +0530

    Update runOnServers.groovy

--- vars/runOnServers.groovy
+++ vars/runOnServers.groovy
@@ -16,3 +16,25 @@ def call(Map config = [:]) {
     }
   )
 }
+
+@NonCPS
+def changeLogs() {
+    String msg = ""
+    String repoUrl = 'BITBUCKET_REPO_URL'
+    def lastId = null
+    def prevId = prevBuildLastCommitId()
+    def changeLogSets = currentBuild.changeSets
+
+    for (int i = 0; i < changeLogSets.size(); i++) {
+        def entries = changeLogSets[i].items
+        for (int j = 0; j < entries.length; j++) {
+            def entry = entries[j]
+            lastId = entry.commitId
+            msg = msg + "${commitInfo(entry)}"
+        }
+    }
+    if (prevId != null && lastId != null) {
+        msg = msg + "\n${repoUrl}/branches/compare/${lastId}..${prevId}#diff\n"
+    }
+    println(msg)
+}
